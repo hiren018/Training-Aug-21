b09c0581b1c53d298c319eec60e116c7e2e0c5fa
Hiren <sanghanihiren746@gmail.com>
2021-08-18 10:12:00 +0530

    practice2

--- SQL/Hiren Sanghani/Day1/practice2.sql
+++ SQL/Hiren Sanghani/Day1/practice2.sql
@@ -2,7 +2,7 @@ CREATE TABLE JobHistory
 (
 EmployeeId int not null PRIMARY KEY,
 StartDate date not null,
-End_Date char CONSTRAINT cheackDate CHECK (End_Date LIKE __/__/____'),
+End_Date char CONSTRAINT cheackDate CHECK (End_Date LIKE '__/__/____'),
 Job_Id char(10) not null,
 Department_Id char(8) not null,
 );
